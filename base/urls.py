370c21a9e495e82b70a1b78b853e43ba50560b10
Remi288 <iamopeoluwaadewale_28@yahoo.com>
2021-04-21 14:09:02 +0100

    Feature:- Implemented Crud funtionality

--- base/urls.py
+++ base/urls.py
@@ -1,8 +1,18 @@
 from django.urls import path
-from .views import TaskList, TaskDetail
+from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete, CustomLoginView, RegisterPage
 from .models import Task
 
+from django.contrib.auth.views import LogoutView
+
 urlpatterns = [
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    path('register/', RegisterPage.as_view(), name='register'),
+
     path('', TaskList.as_view(), name='tasks'),
     path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
+    path('create-task/', TaskCreate.as_view(), name='task-create'),
+    path('edit-task/<int:pk>/', TaskUpdate.as_view(), name='task-edit'),
+    path('delete-task/<int:pk>/', TaskDelete.as_view(), name='task-delete'),
+    
 ]
